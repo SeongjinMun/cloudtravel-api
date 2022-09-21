34c5aa667c3d57ca99295e683c61cff424836dd2
SeongjinMun <sjin217@gmail.com>
2022-09-22 03:47:35 +0900

    TEST
    TEST

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 sudo docker ps -a -q --filter "name=cloudtravel" | grep -q . && docker stop cloudtravel && docker rm cloudtravel | true
 sudo docker rmi sjin217/cloudtravel
 sudo docker pull sjin217/cloudtravel
-docker run -d -p 80:80 --name cloudtravel sjin217/cloudtravel
+docker run -d -p 80:80 -e TZ=Asia/Seoul -v /home/ec2-user/logs:/var/log/apache2 --name cloudtravel sjin217/cloudtravel:latest
 docker rmi -f $(docker images -f "dangling=true" -q) || true
